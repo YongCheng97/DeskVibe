fca63dd4ac76d77266940d899475179494ef9316
daize <e0309481@u.nus.edu>
2020-10-26 22:57:01 +0800

    created team lead svc

---
+++ teamleadservice/src/main/java/com/deskvibe/teamleadservice/TeamleadserviceApplication.java
@@ -0,0 +1,13 @@
+package com.deskvibe.teamleadservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamleadserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamleadserviceApplication.class, args);
+	}
+
+}
